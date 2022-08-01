dc4adbac1964c7c15f7a1394a78e760308b484b0
xfnw <xfnw@riseup.net>
2022-08-01 18:14:56 -0400

    start http stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate warc;
+extern crate rouille;
+use rouille::Response;
+
 fn main() {
-    println!("Hello, world!");
+    rouille::start_server("localhost:8000", move |request| {
+        println!("{:?}", request.url());
+        {
+            if request.url() == "/hello" {
+                return Response::text("hello world");
+            }
+        }
+        return Response::html(
+            "<h1>error: 404 not found</h1>\n\
+                the page you requested is not part of this warc file.",
+        );
+    });
 }
