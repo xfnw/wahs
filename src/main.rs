5a0587143d99357b2b1a30ddf31b444c21979c20
xfnw <xfnw@riseup.net>
2022-08-06 22:30:17 -0400

    read the warc file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
-extern crate warc;
 extern crate rouille;
 use rouille::Response;
 
+extern crate warc;
+use warc::WarcHeader;
+use warc::WarcReader;
+
+extern crate libflate;
+use std::env::args;
+use std::process::exit;
+use std::fs::File;
+use std::io::BufReader;
+use libflate::gzip::MultiDecoder;
+
+fn search_warc(warc: WarcReader<BufReader<MultiDecoder<BufReader<File>>>>, url: &str) {
+    for record in warc.iter_records() {
+        match record {
+            Ok(record) => {},
+            Err(e) => eprintln!("error: {}",e)
+        }
+    }
+}
+
 fn main() {
+    let filename = &match args().nth(1) {
+        Some(h) => h,
+        None => {
+            eprintln!("usage: {} file", args().nth(0).expect("bap."));
+            exit(1);
+        }
+    };
+
+    let file = WarcReader::from_path_gzip(filename).expect("failed to read warc file");
+
+    search_warc(file, "meow");
+
     rouille::start_server("localhost:8000", move |request| {
         println!("{:?}", request.url());
         {
