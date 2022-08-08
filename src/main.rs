aaaac018e4232cc05977e9c8c477a446f4dce246
xfnw <xfnw@riseup.net>
2022-08-07 23:20:51 -0400

    access responses
    still need to chop off the http headers and then convert links

--- src/main.rs
+++ src/main.rs
@@ -30,14 +30,20 @@ fn search_warc(
                 continue;
             }
         };
-        match record.header(WarcHeader::TargetURI) {
-            Some(h) if rem_last(&h).ends_with(&url) => {
-                let body = match std::str::from_utf8(&record.body()) {
-                    Ok(h) => Ok(h.to_string()),
-                    Err(_) => continue,
-                };
-                return body;
-            }
+        match record.header(WarcHeader::WarcType) {
+            Some(rtype) if rtype.eq("response") => match record.header(WarcHeader::TargetURI) {
+                Some(h) if rem_last(&h).ends_with(&url) => {
+                    let body = match std::str::from_utf8(&record.body()) {
+                        Ok(h) => Ok(h.to_string()),
+                        Err(e) => {
+                            println!("{:?}", e);
+                            continue;
+                        }
+                    };
+                    return body;
+                }
+                _ => (),
+            },
             _ => (),
         };
     }
@@ -45,28 +51,33 @@ fn search_warc(
 }
 
 fn main() {
-    let filename = &match args().nth(1) {
-        Some(h) => h,
+    let filename = match args().nth(1) {
+        Some(h) => h.to_string(),
         None => {
             eprintln!("usage: {} file", args().nth(0).expect("bap."));
             exit(1);
         }
     };
 
-    let file = WarcReader::from_path_gzip(filename).expect("failed to read warc file");
-
-    search_warc(file, "png");
-
     rouille::start_server("localhost:8000", move |request| {
+        // FIXME: very slow :(
+        // maybe access with an Arc?
+        let file = WarcReader::from_path_gzip(&filename).expect("failed to read warc file");
+
         println!("{:?}", request.url());
         {
             if request.url() == "/hello" {
                 return Response::text("hello world");
             }
         }
-        return Response::html(
-            "<h1>error: 404 not found</h1>\n\
-                the page you requested is not part of this warc file.",
-        );
+        match search_warc(file, &request.url()) {
+            Ok(body) => return Response::text(body),
+            Err(_) => {
+                return Response::html(
+                    "<h1>error: 404 not found</h1>\n\
+                    the page you requested is not part of this warc file.",
+                )
+            }
+        };
     });
 }
