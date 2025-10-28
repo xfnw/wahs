efc73f25646655141a85379a57cf64af56e805de
xfnw <xfnw@riseup.net>
2025-10-28 17:53:03 -0400

    gencdx: actually get position of gzip segments properly

--- src/bin/gencdx.rs
+++ src/bin/gencdx.rs
@@ -1,4 +1,4 @@
-use libflate::gzip::MultiDecoder as GzipReader;
+use libflate::gzip::Decoder as GzipDecoder;
 use std::{
     borrow::Cow,
     env::args,
@@ -7,27 +7,47 @@ use std::{
 };
 use warc::{WarcHeader, WarcReader};
 
+struct GzipReader<'a, T> {
+    inner: Option<GzipDecoder<LengthReader<'a, T>>>,
+    last_member: &'a AtomicU64,
+}
+
+// adapted from libflate's MultiDecoder Read implementation
+// but less good because libflate makes all the fun stuff private >:(
+impl<T: Read> Read for GzipReader<'_, T> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let Some(ref mut inner) = self.inner else {
+            return Ok(0);
+        };
+        let size = inner.read(buf)?;
+        if size == 0 {
+            let start = inner.as_inner_ref().count.load(Ordering::Relaxed);
+            let mut inner = match GzipDecoder::new(self.inner.take().unwrap().into_inner()) {
+                Ok(i) => i,
+                Err(e) => {
+                    return Err(e);
+                }
+            };
+            self.last_member.store(start, Ordering::Relaxed);
+            let newsize = inner.read(buf);
+            self.inner = Some(inner);
+            newsize
+        } else {
+            Ok(size)
+        }
+    }
+}
+
 struct LengthReader<'a, T> {
     inner: T,
     count: &'a AtomicU64,
-    start_location: &'a AtomicU64,
-    expect_start: bool,
 }
 
 impl<T: Read> Read for LengthReader<'_, T> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         let res = self.inner.read(buf);
         if let Ok(size) = res {
-            let count = self.count.fetch_add(size as u64, Ordering::Relaxed);
-            let start_location = self.start_location.load(Ordering::Relaxed);
-            if count <= start_location {
-                self.expect_start = true;
-            }
-            if self.expect_start && size > 0 && count >= start_location {
-                assert!(start_location == count, "start of gzip skipped over");
-                assert!(buf.starts_with(&[31, 139]), "could not find start of gzip");
-                self.expect_start = false;
-            }
+            self.count.fetch_add(size as u64, Ordering::Relaxed);
         }
         res
     }
@@ -37,31 +57,21 @@ fn main() {
     println!(" CDX a b m s k V g u");
 
     for filename in args().skip(1) {
-        let mut old_offset;
-        let mut offset = 0;
+        let last_member = AtomicU64::new(0);
         let length = AtomicU64::new(0);
-        let start_location = AtomicU64::new(0);
         let file = std::fs::File::open(&filename).expect("open warc file");
-        // XXX: i expected the BufReader on the outside of GzipReader
-        // to mess up our offsets, but it seems to be fine?
-        // if it turns out that it does mess with it, can always just
-        // set the capacity to 1 (to essentially lie about it being
-        // buffered, since WarcReader requires BufRead), but that
-        // causes a 3x performance penalty
-        let file = WarcReader::new(BufReader::new(
-            GzipReader::new(LengthReader {
-                inner: BufReader::with_capacity(1 << 20, file),
-                count: &length,
-                start_location: &start_location,
-                expect_start: true,
-            })
-            .expect("open gzip"),
-        ));
+        let file = WarcReader::new(BufReader::new(GzipReader {
+            inner: Some(
+                GzipDecoder::new(LengthReader {
+                    inner: BufReader::with_capacity(1 << 20, file),
+                    count: &length,
+                })
+                .expect("open gzip"),
+            ),
+            last_member: &last_member,
+        }));
 
         for record in file.iter_records() {
-            old_offset = offset;
-            offset = length.load(Ordering::Relaxed);
-            start_location.store(offset + 8, Ordering::Relaxed);
             let Ok(record) = record else {
                 continue;
             };
@@ -107,11 +117,7 @@ fn main() {
             let k = k.split_once(":").map(|(_, k)| k).unwrap_or(&k);
             // V - file offset (before decompression)
             // should be uppercase, but rust is anal about naming
-            // XXX: 8 is the distance from where it stopped reading
-            // the previous record and the start of this record for
-            // all the warc files i tried, but all my warcs are from
-            // wget, this might be relying on incorrect assumptions :(
-            let v = old_offset + 8;
+            let v = last_member.load(Ordering::Relaxed);
             // g - file name
             let g = &filename;
             // u - ??? missing from the cdx format reference, wget uses warc id
