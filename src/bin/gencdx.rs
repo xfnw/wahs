3f6fbb0ed5c9d6c8a8c2960838215ef1ea2d9d6c
xfnw <xfnw@riseup.net>
2025-10-27 20:04:54 -0400

    gencdx: emit file offsets
    would be better if it could hook into the gzip reader somehow and have
    it provide the offset of the current gzip member, but i could not figure
    out a way to do that, and WarcReader does not expose its inner reader so
    we would not be able to mess with it anyways :(
    
    currently does not do any sort of checking that the offset given is
    actually the start of a gzip member

--- src/bin/gencdx.rs
+++ src/bin/gencdx.rs
@@ -1,20 +1,52 @@
 use libflate::gzip::MultiDecoder as GzipReader;
-use std::{borrow::Cow, env::args, io::BufReader};
+use std::{
+    borrow::Cow,
+    env::args,
+    io::{BufReader, Read},
+    sync::atomic::{AtomicU64, Ordering},
+};
 use warc::{WarcHeader, WarcReader};
 
+struct LengthReader<'a, T> {
+    inner: T,
+    count: &'a AtomicU64,
+}
+
+impl<T: Read> Read for LengthReader<'_, T> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let res = self.inner.read(buf);
+        if let Ok(size) = res {
+            self.count.fetch_add(size as u64, Ordering::Relaxed);
+        }
+        res
+    }
+}
+
 fn main() {
-    // would be nice to include `V` (compressed offset) too, but the warc crate does not give us
-    // that information :(
-    println!(" CDX a b m s k g u");
+    println!(" CDX a b m s k V g u");
 
     for filename in args().skip(1) {
+        let mut old_offset;
+        let mut offset = 0;
+        let length = AtomicU64::new(0);
         let file = std::fs::File::open(&filename).expect("open warc file");
-        let file = WarcReader::new(BufReader::with_capacity(
-            8192,
-            GzipReader::new(BufReader::with_capacity(1 << 20, file)).expect("open gzip"),
+        // XXX: i expected the BufReader on the outside of GzipReader
+        // to mess up our offsets, but it seems to be fine?
+        // if it turns out that it does mess with it, can always just
+        // set the capacity to 1 (to essentially lie about it being
+        // buffered, since WarcReader requires BufRead), but that
+        // causes a 3x performance penalty
+        let file = WarcReader::new(BufReader::new(
+            GzipReader::new(LengthReader {
+                inner: BufReader::with_capacity(1 << 20, file),
+                count: &length,
+            })
+            .expect("open gzip"),
         ));
 
         for record in file.iter_records() {
+            old_offset = offset;
+            offset = length.load(Ordering::Relaxed);
             let Ok(record) = record else {
                 continue;
             };
@@ -58,12 +90,19 @@ fn main() {
                 .header(WarcHeader::PayloadDigest)
                 .unwrap_or(Cow::Borrowed("-"));
             let k = k.split_once(":").map(|(_, k)| k).unwrap_or(&k);
+            // V - file offset (before decompression)
+            // should be uppercase, but rust is anal about naming
+            // XXX: 8 is the distance from where it stopped reading
+            // the previous record and the start of this record for
+            // all the warc files i tried, but all my warcs are from
+            // wget, this might be relying on incorrect assumptions :(
+            let v = old_offset + 8;
             // g - file name
             let g = &filename;
             // u - ??? missing from the cdx format reference, wget uses warc id
             let u = record.warc_id();
 
-            println!("{a} {b} {m} {s} {k} {g} {u}");
+            println!("{a} {b} {m} {s} {k} {v} {g} {u}");
         }
     }
 }
