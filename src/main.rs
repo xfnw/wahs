608388847e4b58bea2fc69d188da2c1c047e33e4
xfnw <xfnw@riseup.net>
2025-12-09 15:28:12 -0500

    Revert "eat base href"
    oh wait probably still need that for stuff that cannot be mangled
    
    This reverts commit da28ca67ef9ddd0ee28f4fe7d3e24dcf681757d8.

--- src/main.rs
+++ src/main.rs
@@ -276,13 +276,6 @@ impl AppState {
                             el.remove();
                             Ok(())
                         }),
-                        element!("base[href]", |el| {
-                            el.remove_attribute("href");
-                            if !el.has_attribute("target") {
-                                el.remove();
-                            }
-                            Ok(())
-                        }),
                     ],
                     ..lol_html::Settings::new()
                 },
